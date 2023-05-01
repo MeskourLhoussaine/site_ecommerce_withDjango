843374adf76e84846d66c0d2eec891080831935b
MeskourLhoussaine <127257257+MeskourLhoussaine@users.noreply.github.com>
2023-05-01 23:19:19 +0100

    informatio html

--- site_ecommerce/our_shop/urls.py
+++ site_ecommerce/our_shop/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, information
 
 
 urlpatterns = [
     path('', index, name='home'),
+    #url dynamique depen de id
+    path('<int:idprod>',information, name='information' ),
 ]
--- site_ecommerce/our_shop/views.py
+++ site_ecommerce/our_shop/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from .models import Produit
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
     produit_obj =Produit.objects.all()
+    item_name = request.GET.get('item-name')
+    if item_name != '' and item_name is not None:
+        produit_obj = Produit.objects.filter(nom__icontains=item_name)
+    paginator = Paginator(produit_obj, 4)
+    page = request.GET.get('page')
+    produit_obj = paginator.get_page(page)
     return render(request,'index.html',{'produit_obj':produit_obj})
+def information(request, idprod):
+    produit_obj = Produit.objects.get(id=idprod)
+    return render(request,'information.html',{'produit_obj':produit_obj})
